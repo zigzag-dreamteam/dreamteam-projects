58237e5ca84a952476724475959d8ea8ba23bc06
afifahrahmak <afifah.ark@gmail.com>
2022-12-06 09:39:04 +0700

    - move register code to app.js - create add product feature

--- app.js
+++ app.js
@@ -7,4 +7,24 @@ app.post('/login', (req, res) => {
     res.status(200).json(access_token)
 })
 
+app.post('/register', async (req, res) => {
+    try {
+        let { email, password } = req.body
+        const user = await User.create({ email, password })
+        res.status(201).json(user)
+    } catch (error) {
+        res.status(500).json({ messsage: 'internal server error' })
+    }
+})
+
+app.post('/products', async (req, res) => {
+    try {
+        let { name, price, stock } = req.body
+        let product = await Product.create({ name, price, stock })
+        res.status(201).json(product)
+    } catch (error) {
+        res.status(500).json({ messsage: 'internal server error' })
+    }
+})
+
 app.listen(3000)
